e08494af38ec2a19bbcfac2a2d620a9787f313e0
hammax <hammax1365@gmail.com>
2015-11-04 16:22:25 +0200

    Add sql create script!

---
+++ Create tables.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE cities
+(
+  id serial NOT NULL,
+  countryname character varying(80),
+  cityname character varying(80),
+  CONSTRAINT cities_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE countries
+(
+  id serial NOT NULL,
+  countryname character varying(80) NOT NULL,
+  countryisocode character varying(3),
+  CONSTRAINT countries_pkey PRIMARY KEY (id)
+);
